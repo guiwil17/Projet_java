5f621760b915ab1509487989d259632f536c4737
Celina Marcelli <celinamarcelli@macbook-air-de-celina.home>
2021-01-08 14:44:19 +0100

    Ajout des méthodes de la classe LigneTableau + test de la méthode de comparaison

--- code/src/Operator/LigneTableau.java
+++ code/src/Operator/LigneTableau.java
@@ -18,7 +18,37 @@ public class LigneTableau
 	private boolean c_limit;
 	private java.util.Date date;
 
+	// Constructeur de l'objet LigneTableau, à partir des paramètres CSV sans les réels et entiers
+	LigneTableau(boolean box_conveyor, boolean part_conveyor, boolean grab, boolean c_plus, boolean auto, boolean manual, boolean emergency_stop, boolean reset_button, boolean start, boolean stop, boolean part_at_place, boolean box_at_place, boolean detected, boolean c_limit, java.util.Date date)
+		{
+		this.box_conveyor = box_conveyor;
+		this.part_conveyor = part_conveyor;
+		this.grab = grab;
+		this.c_plus = c_plus;
+		this.auto = auto;
+		this.manual = manual;
+		this.emergency_stop = emergency_stop;
+		this.reset_button = reset_button;
+		this.start = start;
+		this.stop = stop;
+		this.part_at_place = part_at_place;
+		this.box_at_place = box_at_place;
+		this.detected = detected;
+		this.c_limit = c_limit;
+		this.date = date;
+		}
 	
+	public boolean is_equal_to(LigneTableau l2)
+		{
+		boolean b = false;
+		if(box_conveyor == l2.get_box_conveyor() && part_conveyor == l2.get_part_conveyor() && grab == l2.get_grab() && c_plus == l2.get_c_plus() && auto == l2.get_auto() && manual == l2.get_manual() && emergency_stop == l2.get_emergency_stop() && reset_button == l2.get_reset_button() && start == l2.get_start() && stop == l2.get_stop() && part_at_place == l2.get_part_at_place() && box_at_place == l2.get_box_at_place() && detected == l2.get_detected() && c_limit == l2.get_c_limit())
+			{
+			b = true;
+			}
+		return b;
+		}
+	
+	// Setteurs des paramètres de chaque ligne du CSV	
 	public void set_box_conveyor(boolean b)
 		{
 		box_conveyor = b;
@@ -94,5 +124,80 @@ public class LigneTableau
 		date = d;
 		}
 	
-
+// Getteurs des paramètres de chaque ligne du CSV
+	
+	public boolean get_box_conveyor()
+		{
+		return box_conveyor;
+		}
+	
+	public boolean get_part_conveyor()
+		{
+		return part_conveyor;
+		}
+	
+	public boolean get_grab()
+		{
+		return grab;
+		}
+	
+	public boolean get_c_plus()
+		{
+		return c_plus;
+		}
+	
+	public boolean get_auto()
+		{
+		return auto;
+		}
+	
+	public boolean get_manual()
+		{
+		return manual;
+		}
+	
+	public boolean get_emergency_stop()
+		{
+		return emergency_stop;
+		}
+	
+	public boolean get_reset_button()
+		{
+		return reset_button;
+		}
+	
+	public boolean get_start()
+		{
+		return start;
+		}
+	
+	public boolean get_stop()
+		{
+		return stop;
+		}
+	
+	public boolean get_part_at_place()
+		{
+		return part_at_place;
+		}
+	
+	public boolean get_box_at_place()
+		{
+		return box_at_place;
+		}
+	
+	public boolean get_detected()
+		{
+		return detected;
+		}
+	
+	public boolean get_c_limit()
+		{	
+		return c_limit;
+		}
+	
+	public java.util.Date get_date()
+		{
+		return date;
+		}
 	}
