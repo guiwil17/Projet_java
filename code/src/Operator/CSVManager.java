2ad91409a89bb469f310668a613eb51902dd1100
Guillaume <49609994+guiwil17@users.noreply.github.com>
2021-01-12 10:31:18 +0100

    Update CSVManager.java

--- code/src/Operator/CSVManager.java
+++ code/src/Operator/CSVManager.java
@@ -187,16 +187,6 @@ public class CSVManager {
 
 	}
 
-public class CSVManager 
-	{
-	Vector<LigneTableau> vector = new Vector<LigneTableau>();
-	
-	public static void main(String[] args) 
-		{
-		// TODO Auto-generated method stub
-
-		}
-	
 	public void supprimer_doublons()
 		{
 		Vector<LigneTableau> vector2 = new Vector<LigneTableau>();
@@ -227,5 +217,4 @@ public class CSVManager
 			System.out.print(vector.get(indice).to_string());
 			}
 		}
-
-	}
+}
