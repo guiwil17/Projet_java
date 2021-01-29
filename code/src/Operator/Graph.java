8baaf1cf7793a55caabc59caeecebc748ae884b4
Guillaume <49609994+guiwil17@users.noreply.github.com>
2021-01-29 19:07:52 +0100

    Add files via upload

--- code/src/Operator/Graph.java
+++ code/src/Operator/Graph.java
@@ -1,8 +1,26 @@
 package Operator;
+import java.util.Vector;
+import com.google.common.graph.MutableValueGraph;
+import com.google.common.graph.ValueGraphBuilder;
 
-public class Graph {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Graph 
+	{
+	private MutableValueGraph<GraphNode, String> graphe;
+	Graph()
+		{
+		graphe = ValueGraphBuilder.directed().build();
+		}
+	
+	public void split(Vector<String> v)
+		{
+		int taille = v.size();
+		for(int i=0; i<taille; i++)
+			{
+			String[] element = v.elementAt(i).split(";");
+			GraphNode a = new GraphNode(element[0]);
+			GraphNode b = new GraphNode("test");
+			graphe.putEdgeValue(a, b, element[1]);
+			}
+		System.out.println(graphe.toString());
+		}
 	}
-}
---
+++ code/src/Operator/test_graph.java
@@ -0,0 +1,16 @@
+package Operator;
+import java.util.Vector;
+
+
+
+public class test_graph 
+	{
+	public static void main(String[] args) 
+		{
+		Graph g = new Graph();
+		Vector<String> v=new Vector<String>();
+		v.add("FE_box_at_place;1µs;");
+		v.add("RE_box_at_place;5µs;");
+		g.split(v);
+		}
+	}
